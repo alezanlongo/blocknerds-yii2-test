a06e4215d19b939ec72c03ebbd9b50f8afad42ad
Nahuel Bulian <nbulian@gmail.com>
2021-05-07 16:51:47 -0300

    Adding missin files for unit test for Collection & Image

---
+++ yii-application/common/fixtures/ImageFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace common\fixtures;
+
+use yii\test\ActiveFixture;
+
+class ImageFixture extends ActiveFixture
+{
+    public $modelClass = 'common\models\Image';
+}
--- yii-application/common/tests/_data/image.php
+++ yii-application/common/tests/_data/image.php
@@ -6,5 +6,9 @@ return [
         'author' => 'some author',
         'description' => 'some description',
         'url' => 'https://images.unsplash.com/photo-1618757850520-6e3e48d07746?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjQ2NTl8MHwxfHNlYXJjaHw0fHxhc2R8ZW58MHx8fHwxNjIwMTUwNTU5&ixlib=rb-1.2.1&q=80&w=400',
+        'created_by' => 1,
+        'updated_by' => 1,
+        'created_at' => '1402312317',
+        'updated_at' => '1402312317'
     ],
 ];
